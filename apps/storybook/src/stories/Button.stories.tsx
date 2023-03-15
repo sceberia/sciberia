e8a17c07bfcbf971d25e401d0fbe9453d13dbb0f
1Suro1 <timu.zaxarov@gmail.com>
2023-03-15 18:57:05 +0900

    test actions

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -3,7 +3,6 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from 'ui'
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/Button',
   component: Button,
