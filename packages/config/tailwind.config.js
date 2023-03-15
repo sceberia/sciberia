9237afa8ed3013dc77b93e442e3936fc48ba53e0
1Suro1 <timu.zaxarov@gmail.com>
2023-03-15 16:16:16 +0900

    storybook add button

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
     ],
     theme: {
       extend: {},
-    },
-    colors: {
-      "custom-black": "#070918",
-      "primary": "#0040F4",
-      "primary-hover": "#0438CA",
-      "secondary": "#314968",
+      colors: {
+        "custom-white": "#ffffff",
+        "custom-black": "#070918",
+        "primary": "#0040F4",
+        "primary-hover": "#0438CA",
+        "secondary": "#314968",
+      },
     },
     plugins: [],
 }
--- packages/ui/components/button/Button.tsx
+++ packages/ui/components/button/Button.tsx
@@ -1,4 +1,62 @@
-import * as React from "react";
-export const Button = () => {
-  return <button className="text-3xl font-bold underline text-gray-600">Boopsadasdas</button>;
+import React from 'react';
+
+interface ButtonProps {
+  /**
+   * Первичный?
+   */
+  primary?: boolean;
+  /**
+   * Какой цвет задать 
+   * //TODO сделать массив цветов
+   */
+  backgroundColor?: string;
+  /**
+   * Какого размера кнопка
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Надпись кнопки
+   */
+  label: string;
+  /**
+   * Обработчик нажатия
+   */
+  onClick?: () => void;
+}
+
+const getSizeClasses = (size:any) => {
+  switch(size){
+    case 'small': {
+      return 'p-32 py-2.5'
+    }
+    case 'large': {
+      return 'px-6 py-3'
+    }
+    default: {
+      return 'px-5 py-2.5'
+    }
+  }
+}
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const sizeClass = getSizeClasses(size)
+  const mode = primary ? 'bg-primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['text-custom-white rounded-xl', sizeClass, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
 };
