e4bbcaa1c471b442c458a885913ad93f8c877ff9
1Suro1 <timu.zaxarov@gmail.com>
2023-03-15 18:58:49 +0900

    maybe fix build

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -8,7 +8,7 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
